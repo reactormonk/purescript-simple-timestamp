e73d6bcbf2cea00d1f0537279c2175c9f8831009
Mark Eibes <mark.eibes@gmail.com>
2019-10-10 11:31:09 +0200

    Parse any precision millis and roundtrip up to 3 millis

--- src/Timestamp.purs
+++ src/Timestamp.purs
@@ -4,20 +4,25 @@ import Prelude
 
 import Control.Alt ((<|>))
 import Control.Monad.Error.Class (throwError)
-import Data.DateTime (DateTime)
-import Data.Either (Either(..))
+import Data.DateTime (DateTime, millisecond, time)
+import Data.Either (Either(..), either)
+import Data.Enum (fromEnum)
 import Data.Formatter.DateTime (Formatter, FormatterCommand(..), format, unformat)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Data.List (fromFoldable)
+import Data.List (List, fromFoldable)
 import Data.Newtype (class Newtype)
+import Data.String.Regex (Regex, regex)
+import Data.String.Regex as Regex
+import Data.String.Regex.Flags (noFlags)
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Now (nowDateTime)
 import Foreign (ForeignError(..))
+import Partial.Unsafe (unsafeCrashWith)
 import Simple.JSON (class ReadForeign, class WriteForeign, readImpl, writeImpl)
 
-iso8601LongFormat :: Formatter
-iso8601LongFormat = fromFoldable
+mkIso8601Format :: Array FormatterCommand -> List FormatterCommand
+mkIso8601Format milliParser = fromFoldable $
   [ YearFull
   , Placeholder "-"
   , MonthTwoDigits
@@ -29,26 +34,29 @@ iso8601LongFormat = fromFoldable
   , MinutesTwoDigits
   , Placeholder ":"
   , SecondsTwoDigits
-  , Placeholder "."
-  , Milliseconds
-  , Placeholder "Z"
+  ] <> milliParser <>
+  [ Placeholder "Z"
   ]
 
 iso8601ShortFormat :: Formatter
-iso8601ShortFormat = fromFoldable
-  [ YearFull
-  , Placeholder "-"
-  , MonthTwoDigits
-  , Placeholder "-"
-  , DayOfMonthTwoDigits
-  , Placeholder "T"
-  , Hours24
-  , Placeholder ":"
-  , MinutesTwoDigits
-  , Placeholder ":"
-  , SecondsTwoDigits
-  , Placeholder "Z"
-  ]
+iso8601ShortFormat = mkIso8601Format []
+
+iso86011DigitMillisFormat :: Formatter
+iso86011DigitMillisFormat = mkIso8601Format [ Placeholder ".", MillisecondsShort ]
+
+iso86012DigitsMillisFormat :: Formatter
+iso86012DigitsMillisFormat = mkIso8601Format [ Placeholder ".", MillisecondsTwoDigits]
+
+iso8601LongFormat :: Formatter
+iso8601LongFormat = mkIso8601Format [ Placeholder ".", Milliseconds ]
+
+stripMillis :: String -> String
+stripMillis =
+  Regex.replace theRegex replacement
+  where
+    replacement = ".$1$2$3"
+    theRegex = regex "\\.(\\d)(\\d)(\\d)\\d+" noFlags
+      # either unsafeCrashWith identity
 
 newtype Timestamp = Timestamp DateTime
 derive newtype instance eqTimestamp :: Eq Timestamp
@@ -56,7 +64,14 @@ derive newtype instance ordTimestamp :: Ord Timestamp
 instance readTimestamp :: ReadForeign Timestamp where
   readImpl v = do
     s <- readImpl v
-    case unformat iso8601LongFormat s <|> unformat iso8601ShortFormat s of
+    let
+      as = flip unformat s
+      parsed =  as iso8601ShortFormat
+            <|> as iso86011DigitMillisFormat
+            <|> as iso86012DigitsMillisFormat
+            <|> as iso8601LongFormat
+            <|> unformat iso8601LongFormat (s # stripMillis)
+    case parsed of
       Right d -> pure (Timestamp d)
       Left e -> throwError $ pure $ ForeignError e
 
@@ -72,4 +87,11 @@ nowTimestamp :: ∀ m. MonadEffect m => m Timestamp
 nowTimestamp = liftEffect $ Timestamp <$> nowDateTime
 
 printTimestamp :: Timestamp -> String
-printTimestamp (Timestamp dt) = format iso8601LongFormat dt
+printTimestamp (Timestamp dt) = dt # format milliDigits
+  where
+    millis = dt # time # millisecond # fromEnum
+    milliDigits = case millis of
+      0                      -> iso8601ShortFormat
+      ms | ms `mod` 100 == 0 -> iso86011DigitMillisFormat
+      ms | ms `mod` 10  == 0 -> iso86012DigitsMillisFormat
+      _                      -> iso8601LongFormat
